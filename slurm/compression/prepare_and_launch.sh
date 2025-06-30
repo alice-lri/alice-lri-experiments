c536922aa6a3a3339062c25079a609e60c5ef680
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-06-30 11:46:21 +0200

    Changed skip training logic

--- slurm/compression/prepare_and_launch.sh
+++ slurm/compression/prepare_and_launch.sh
@@ -4,19 +4,16 @@ cd "$(dirname "$0")" || exit
 
 CONTAINER_PATH="../../container.sif"
 
+if [[ $# -gt 0 ]]; then
+  SKIP_TRAINING=1
+fi
+
 source ../helper/paths.sh
 source ../helper/multi_batch_job_header.sh
 
 module load cesga/system apptainer/1.2.3
 apptainer exec "$CONTAINER_PATH" ./prepare.sh "$BASE_DB_DIR" "$ACTUAL_DB_DIR"
 
-SKIP_TRAINING=0
-for arg in "$@"; do
-  if [[ "$arg" == "--skip-training" ]]; then
-    SKIP_TRAINING=1
-    break
-  fi
-done
 
 if [[ "$SKIP_TRAINING" -eq 0 ]]; then
     echo "Launching train job..."
