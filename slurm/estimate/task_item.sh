3f53d0c1b030aff7bb106b60aed02afb4da1059d
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-07-08 19:55:13 +0200

    Fixed source order in task_item.sh

--- slurm/estimate/task_item.sh
+++ slurm/estimate/task_item.sh
@@ -9,9 +9,9 @@ TASK_COUNT=$4
 EXEC_DIR=$(dirname "$EXECUTABLE_PATH")
 EXEC_FILE=$(basename "$EXECUTABLE_PATH")
 
-cd "$EXEC_DIR" || { echo "Failed to cd into $EXEC_DIR"; exit 1; }
-
 source ../helper/prepare_task_item.sh
+
+cd "$EXEC_DIR" || { echo "Failed to cd into $EXEC_DIR"; exit 1; }
 
 echo "Running task $TASK_INDEX of $TASK_COUNT..."
 ./"$EXEC_FILE" "$TASK_INDEX" "$TASK_COUNT" 2>&1 | tee "${TRACE_FILE_PATH}"
