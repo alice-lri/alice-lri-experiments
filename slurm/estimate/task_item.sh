b447a3486a67cf0b04591320b6b208c7bad3763c
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-08-14 18:15:02 +0200

    More things to easy debugging

--- slurm/estimate/task_item.sh
+++ slurm/estimate/task_item.sh
@@ -14,6 +14,6 @@ source ../helper/prepare_task_item.sh
 cd "$EXEC_DIR" || { echo "Failed to cd into $EXEC_DIR"; exit 1; }
 
 echo "Running task $TASK_INDEX of $TASK_COUNT..."
-./"$EXEC_FILE" "$TASK_INDEX" "$TASK_COUNT" 2>&1 | tee "${TRACE_FILE_PATH}"
+./"$EXEC_FILE" "$TASK_INDEX" "$TASK_COUNT" 2>&1 | tee "${TRACE_FILE_PATH}" || { echo "Failed task: $TASK_INDEX"; exit 1; }
 
 touch "${SUCCESS_FILE_PATH}"
